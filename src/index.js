df981cd635f0e83744ecb376766d9b404509dfb2
Patrick_B <pb5191@gmail.com>
2023-06-04 10:39:57 -0400

    Modify On/Off feature

--- src/index.js
+++ src/index.js
@@ -10,47 +10,34 @@ function order() {
   const { made } = newOrder.makeCoffee(1);
   output.innerHTML = `${made} is waiting for you!`;
 }
-
+function handleClick(evnt) {
+  console.log("Clicked! ", evnt.target.textContent);
+  console.log(evnt.target.attributes.type);
+}
 //* Actions
 //* =======================
 // Turning the machine ON or OFF
 document.querySelector("#onOff").addEventListener("click", () => {
-  const coffeeButtons = document.querySelectorAll(".coffee-buttons > button");
-  const controller = new AbortController();
-  // Just for test
-  const showID = (buttonClicked) => {
-    console.log("buttonID :>> ", buttonClicked.id);
-  };
+  const coffeeButtons = document.querySelectorAll("#coffeeSelectors > button");
+  const container = document.getElementById("coffeeSelectors");
 
-  // Listeners clean up
+  container.removeEventListener("click", handleClick);
   for (const element of coffeeButtons) {
-    if (element.style.color === "antiquewhite") {
-      element.removeEventListener("click", function () {});
-    }
+    element.classList.toggle("inactive");
+    element.classList.toggle("active");
+  }
+  if (coffeeButtons[0].classList.contains("active")) {
+    container.addEventListener("click", handleClick);
+  } else {
+    container.removeEventListener("click", handleClick);
   }
 
-  document.querySelector("#makeCoffee").removeEventListener("click", order);
   document.querySelector(".right-side").classList.toggle("invisible");
-  for (const element of coffeeButtons) {
-    element.style.color === "darkgrey" || !element.style.color
-      ? (element.style.color = "antiquewhite")
-      : (element.style.color = "darkgrey");
-    element.addEventListener("click", function () {
-      if (element.style.color === "antiquewhite") {
-        showID(this);
-        element.removeEventListener("click", function () {});
-      } else {
-        controller.abort();
-      }
-    });
-  }
+
   const start = new onOff();
   // Setting initial inventory in session storage
   start.setInventory();
   start.getInventory();
   start.clearDisplay();
   start.displayInventory();
-
-  // Making one coffee
-  //   document.querySelector("#makeCoffee").addEventListener("click", order);
 });
